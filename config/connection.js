86c499e0cfd97a7bf973fa479183d0fcac7dc993
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-03-22 15:15:46 -0500

    Setting up app with JawsDB credentials

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: '127.0.0.1',
+    host: 'r4wkv4apxn9btls2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
     port: 3306,
   }
