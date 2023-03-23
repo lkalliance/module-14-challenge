4fe72a6d140f2a5ea1b831fdaf73b8db2e1c254b
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-03-23 14:44:09 -0500

    Added screenshots to README

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,9 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host via JawsDB for Heroku
-    host: 'mysql://p2caqq4b3jiivaiw:fi65e2tz4hx22449@r4wkv4apxn9btls2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/chzr4ss9vajiwphj',
+    // host via JawsDB for Heroku
+    // host: 'mysql://p2caqq4b3jiivaiw:fi65e2tz4hx22449@r4wkv4apxn9btls2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/chzr4ss9vajiwphj',
+    host: '127.0.0.1',
     dialect: 'mysql',
     port: 3306,
   }
